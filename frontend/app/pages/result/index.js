cd94c5ca8efffcfd6ba1d8453536d2eabaf1b110
Tanaka Kent <tanaken0304.0123@gmail.com>
2021-03-24 14:14:57 +0900

    Add test contents into each routing page

---
+++ frontend/app/pages/result/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.scss'
+
+export default function Result() {
+  return (
+    <div className={styles.container}>
+
+      ここは結果のページ
+
+
+    </div>
+  )
+}
