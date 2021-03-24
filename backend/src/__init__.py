ea942ec175638ecee09e750bd34b9b9d9bc43e26
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-24 17:18:33 +0900

    db setting path

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('src.db.config')
+
+db = SQLAlchemy(app)
 
 import src.hello
