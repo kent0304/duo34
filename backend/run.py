2a34a57917830a843814980e3a261f5a77dcfd82
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-30 22:04:58 +0900

    add header for cors

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,18 @@
 from src import app, db
 from flask_migrate import Migrate
+from flask_cors import CORS
 
+app.config['JSON_AS_ASCII'] = False
 db.init_app(app)
 Migrate(app, db)
+CORS(app)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allaw-Headers', 'Content-Type, Authorization')
+  response.headers.add('Access-Control-Allaw-Methods', 'GET, PUT, POST, DELETE')
+  return response
 
 def main():
   app.run(port=5000, debug=True)
