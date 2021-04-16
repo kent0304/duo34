e66310c4bd0888e4690c11c9d692f31c99ad5cc0
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-04-16 18:26:36 +0900

    implement quiz flow

--- frontend/app/pages/questions/[pid]/index.js
+++ frontend/app/pages/questions/[pid]/index.js
@@ -1,30 +1,55 @@
+import React, { useState } from 'react';
 import { useRouter } from 'next/router'
 import styles from '../../../styles/Home.module.scss';
 import Layout from '../../components/Layout';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import TestForm from '../../components/TestForm';
 
-function ConductionID(props) {
+function ConductionQuiz(props) {
   const router = useRouter();
   const { pid } = router.query;
   const { tests } = props.state.questions;
+  const [numberOfCorrects, setCorrects] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = () => {
+    setCurrentIndex(currentIndex + 1);
+  }
+
+  const handleSubmit = () => {
+    console.log('submit')
+    router.push('/')
+  }
+
+  const renderButton = () => {
+    if(currentIndex < tests.length - 1) {
+      return (
+        <button onClick={handleClick}>回答</button>
+      )
+    }else {
+      return (
+        <button onClick={handleSubmit}>採点</button>
+      )
+    }
+  }
 
   const renderTests = () => {
-    return (
-      _.map(tests, test => (
-        <div key={test.id} value={test.id}>
-          <p>問題番号：{test.question_number}</p>
-          <p>英語text：{test.english_text}</p>
-          <p>日本語text：{test.japanese_text}</p>
+    const test = tests[currentIndex];
+      return (
+        <div>
+          <p>No. {currentIndex + 1}</p>
+          <p>Question: {test.japanese_text}</p>
+          <p>Answer:<input type='text' /></p>
+          {renderButton()}
         </div>
-      ))
-    )
+      )
   }
 
   return (
     <Layout>
       <div className={styles.container}>
-        <p>実施：{ pid }</p>
+        <p>実施番号：{ pid }</p>
         {renderTests()}
       </div>
     </Layout>
@@ -37,4 +62,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, null)(ConductionID);
+export default connect(mapStateToProps, null)(ConductionQuiz);
